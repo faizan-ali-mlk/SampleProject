e6ad8f6456284bf388583d855004ae2b105a5178
Faizan Ali <precious.logic@gmail.com>
2016-07-06 17:30:14 +0400

    Test

---
+++ TestProject/TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by Faizan Ali on 2/3/16.
+//  Copyright © 2016 Sofwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestProject/TestProject/TestTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestTableViewController.h
+//  TestProject
+//
+//  Created by Faizan Ali on 2/3/16.
+//  Copyright © 2016 Sofwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestTableViewController : UITableViewController
+
+@end
